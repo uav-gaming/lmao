c097ee92698e1f5271cb963f356a84d930c2d63b
Tianjiao Huang <git@tjhu.dev>
2022-07-09 21:29:25 -0400

    init nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Kmer counter";
+
+  inputs = {
+    mars-std.url = "github:mars-research/mars-std";
+  };
+
+  outputs = { self, mars-std, ... }: let
+  # System types to support.
+  supportedSystems = [ "x86_64-linux" ];
+
+  in mars-std.lib.eachSystem supportedSystems (system: let
+    pkgs = mars-std.legacyPackages.${system};
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go_1_18
+        ];  
+      };
+    }
+  );
+}
