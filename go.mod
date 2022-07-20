cd64c8bc647db7052514776163b5eaa8777b897a
Tianjiao Huang <git@tjhu.dev>
2022-07-20 01:01:36 -0700

    [WIP]add a basic discord bot
    error sending heartbeat to gateway wss://gateway.discord.gg ... broken pipe

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/tjhu/lmao
 
 go 1.18
 
-require github.com/aws/aws-lambda-go v1.32.1 // indirect
+require github.com/sirupsen/logrus v1.9.0
+
+require (
+	github.com/aws/aws-lambda-go v1.32.1 // indirect
+	github.com/bwmarrin/discordgo v0.25.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- lmao.go
+++ lmao.go
@@ -2,19 +2,81 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/bwmarrin/discordgo"
+	log "github.com/sirupsen/logrus"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+var s *discordgo.Session
+
+func init() { flag.Parse() }
+
+func init() {
+	var err error
+	discord_token := os.Getenv("DISCORD_TOKEN")
+	if len(discord_token) <= 0 {
+		log.Fatal("env DISCORD_TOKEN not set.")
+	}
+	s, err = discordgo.New("Bot " + discord_token)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "basic-command",
+			Description: "Basic command",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Hey there! Congratulations, you just executed your first slash command",
+				},
+			})
+		},
+	}
+)
+
+func init() {
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func HandleRequest(ctx context.Context, body []byte) (string, error) {
+	log.Info("Got request: ", body)
+	return "Hello world!", nil
 }
 
 func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Info("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+	err := s.Open()
+	if err != nil {
+		log.Fatal("Cannot open the session: ", err)
+	}
+
+	log.Println("Adding commands...")
+	for _, v := range commands {
+		_, err := s.ApplicationCommandCreate(s.State.User.ID, os.Getenv("DISCORD_GUILD"), v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+	}
+
+	defer s.Close()
+
 	lambda.Start(HandleRequest)
 }
