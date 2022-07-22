f2bc3bdad1ce72f63ea5c250e2beb2deb3966cca
Tianjiao Huang <git@tjhu.dev>
2022-07-22 15:57:29 -0700

    verificiation succeed!
    fixed VerifyRequest

--- lmao/security.go
+++ lmao/security.go
@@ -22,14 +22,23 @@ type VerificationError struct {
 }
 
 func VerifyRequest(request Request) bool {
-	signature := request.Headers["X-Signature-Ed25519"]
+	// TODO: get rid of the case sensitivity.
+	signature := request.Headers["x-signature-ed25519"]
+	if len(signature) <= 0 {
+		logrus.Warn("Http header x-signature-ed25519 not set")
+		return false
+	}
 	decoded_signature, err := hex.DecodeString(signature)
 	if err != nil {
 		logrus.Warn("Invliad request signature: ", signature)
 		return false
 	}
 
-	timestamp := request.Headers["X-Signature-Timestamp"]
+	timestamp := request.Headers["x-signature-timestamp"]
+	if len(timestamp) <= 0 {
+		logrus.Warn("Http header x-signature-timestamp not set")
+		return false
+	}
 	body := request.Body
 
 	return ed25519.Verify(discord_public_key, []byte(timestamp+body), decoded_signature)
